f0e2e8b46e8f24768000eb6f56ae9513f4c604a5
Joe OBrien <joe@objo.com>
2018-08-15 18:26:26 -0400

    added books scaffold / extracted song form into partial

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_08_232436) do
+ActiveRecord::Schema.define(version: 2018_08_15_220942) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.integer "pages"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "songs", force: :cascade do |t|
     t.string "name"
